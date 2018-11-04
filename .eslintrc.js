ea23dcc8af344b046f12de67b3ec1c8849db406d
Victor Wang(wang shaojie) <wang_shaojie@icloud.com>
2018-11-04 16:35:46 +0800

    initial commmit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "extends": ["airbnb"],
+  "plugins": [
+    "react"
+  ],
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "webextensions": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 7,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "rules": {
+    "react/jsx-filename-extension": "off",
+    "semi": ["error", "never"],
+    "react/prop-types": "off"
+  }
+};
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# verbose-broccoli
+# Chrome Extension Starter Kit
+
+## Usage
+- Clone this repository
+- Run `yarn & yarn watch`
+- Open Chrome. Go to `chrome://extensions` management page, Click **Load unpacked** button, then select extension folder of this project on your disk.
